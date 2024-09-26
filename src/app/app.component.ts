9b5f71a61ea511f852ff8b11e94f574e9e3a6c53
MarineGiroux <marinegiroux71@gmail.com>
2024-09-26 16:54:34 +0200

    directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,52 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ArticleComponentComponent } from './article-component/article-component.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet,
-    ArticleComponentComponent
+    CommonModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'Marine';
+  
+  articles = [
+    { 
+      title: 'Angular 16: Les nouveautés', 
+      author: 'Alice', 
+      content: 'Les nouveautés d\'Angular 16 incluent...', 
+      image: 'https://via.placeholder.com/350x150',
+      isPublished: false, 
+      comment: '', 
+      likes: 200 
+    },
+    { 
+      title: 'Développer une API REST', 
+      author: 'Bob', 
+      content: 'Développer une API REST nécessite...', 
+      image: 'https://via.placeholder.com/350x150',
+      isPublished: true, 
+      comment: '', 
+      likes: 75 
+    },
+    { 
+      title: 'Pourquoi TypeScript est essentiel ?', 
+      author: 'Charlie', 
+      content: 'TypeScript apporte de la robustesse...', 
+      image: 'https://via.placeholder.com/350x150',
+      isPublished: true, 
+      comment: '', 
+      likes: 100 
+    }
+  ];
+
+  get publishedArticles() {
+    return this.articles.filter(article => article.isPublished);
+  }
+
 }
